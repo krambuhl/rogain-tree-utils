1ae799b243890425f57812ababfe46008bd5c663
Evan Krambuhl <thekrambuhl@gmail.com>
2016-02-06 00:42:52 -0800

    init

---
+++ test/splitTree.test.js
@@ -0,0 +1,15 @@
+var test = require('tape');
+var templates = require('./fixtures/templates.json');
+var splitTree = require('../dist').splitTree;
+
+test.skip('splitTree(Template)', function(t) {
+  t.plan(0);
+});
+
+test.skip('splitTree(Template, { type })', function(t) {
+  t.plan(0);
+});
+
+test.skip('splitTree(Template, { type, name })', function(t) {
+  t.plan(0);
+});
