46b877f980c6862a250d5b574fbb8ac80531b2b9
Evan Krambuhl <thekrambuhl@gmail.com>
2016-02-07 19:28:18 -0800

    better tests, clearer code

--- test/splitTree.test.js
+++ test/splitTree.test.js
@@ -5,33 +5,43 @@ var splitTree = require('../dist').splitTree;
 var testTree = templates.Template.children;
 
 test('splitTree(tree)', function(t) {
-  t.plan(1);
+  t.plan(3);
+  
   var trees = splitTree(testTree);
+
   t.equal(trees.length, testTree.length);
+  t.equal(trees[0][0].type, testTree[0].type);
+  t.equal(trees[1][0].type, testTree[1].type);
 });
 
 test('splitTree(tree, { }) :: middle', function(t) {
-  t.plan(2);
+  t.plan(3);
 
   var trees = splitTree(testTree, { type: 'helper', name: 'NonEmpty' });
 
   t.equal(trees[0][0].type, testTree[0].type);
   t.equal(trees[1][0].type, testTree[2].type);
+  t.equal(trees[1][1].type, testTree[3].type);
 });
 
 test('splitTree(tree, { }) :: first', function(t) {
-  t.plan(2);
+  t.plan(4);
 
   var trees = splitTree(testTree, { type: 'tag', tagName: 'p' });
 
   t.equal(trees[0].length, testTree.length - 1);
   t.equal(trees[0][0].type, testTree[1].type);
+  t.equal(trees[0][1].type, testTree[2].type);
+  t.equal(trees[0][2].type, testTree[3].type);
 });
 
 test('splitTree(tree, { }) :: last', function(t) {
-  t.plan(2);
+  t.plan(4);
 
   var trees = splitTree(testTree, { type: 'tag', tagName: 'div' });
+  
   t.equal(trees[0].length, testTree.length - 1);
   t.equal(trees[0][0].type, testTree[0].type);
+  t.equal(trees[0][1].type, testTree[1].type);
+  t.equal(trees[0][2].type, testTree[2].type);
 });
